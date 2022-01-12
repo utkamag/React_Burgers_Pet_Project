11a2bfd76b3d23c35ef97519dcc7bba2c5f973f8
Егор Вихарев <92789705+utkamag@users.noreply.github.com>
2022-01-12 13:42:40 +0400

    Update Order.js

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,14 @@
 import React, {useState} from "react";
+import sampleRestaurants from "../sample-restaurants";
+import restaurants from "../sample-restaurants";
 
 
 function Order() {
+
     return (
         <div className="order-wrap">
             <h2>Ваш заказ</h2>
+
 
         </div>
     )
